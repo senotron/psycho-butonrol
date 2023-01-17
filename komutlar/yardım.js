f09df8b0ee72d968bb5cd0d477b9bed54964662e
Glitch (harsh-married-toucan) <none>
2023-01-17 15:31:19 +0000

    🏁🌑 Checkpoint ./psychoconf/config.txt:52832527/8 ./psychoconf/config.json:52832527/658 ./komutlar/yardım.js:52832527/5396

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,37 +1,49 @@
-const { MessageEmbed,CommandInteraction,Client,Permissions } = require("discord.js");
+const { MessageEmbed,Client,CommandInteraction,MessageActionRow,MessageButton,Permissions } = require("discord.js");
+const {butonrol1,buton1isim,butonrol2,buton2isim} = require("./psychoconf/config.json");
+
 module.exports = {
-    name:"yardım",
-    description: 'Yardım Menüsü',
+    name:"gönder",
+    description: 'gönder',
     type:1,
-    options:[],
+    options:[ 
+        {
+            name:"açıklama",
+            description:"Embed mesajın açıklaması",
+            type:3,
+            required:true
+        },
+       ],
 /**
  * 
  * @param {Client} client 
  * @param {CommandInteraction} interaction 
  */
     run: async (client, interaction) => {
-        interaction.reply({embeds:[
-            {
-                title: "Gweep Creative Komutlar",
-                description: "Gweep Creative Bot'un komutlarını gösterir",
-                color: 0x00ff00,
-                fields:[
-                    {name: "/ayarlar", value: "Sunucu Ayarları", inline: true},
-                    {name: "/ban", value: "Kullanıcıyı banlar", inline: true},
-                    {name: "/ban-sistem", value: "Ban Sistemi", inline: true},
-                    {name: "/unban", value: "Kullanıcıyı banını açar", inline: true},
-                    {name: "/kick", value: "Kullanıcıyı Sunucudan atar", inline: true},
-                    {name: "/kick-sistem", value: "Kick Sistemi", inline: true},
-                    {name: "/küfür-engel", value: "Küfür Engelleme Sistemi", inline: true},
-                    {name: "/kelime-engel", value: "Kelime Engelleme Sistemi", inline: true},
-                    {name: "/link-engel", value: "Link Engelleme Sistemi", inline: true},
-                    {name: "/nuke", value: "Kanalı yeniden oluşturur", inline: true},
-                    {name: "/sil", value: "Belirtilen miktarda mesjaı siler", inline: true},
-                    {name: "/mod-log", value: "Log mesjalarnın gideceği kanal", inline: true},
-
-                ],
-                thumbnail: {url: client.user.avatarURL({dynamic:true})},
-            }
-        ]});
+            const {guild, options} = interaction;
+            const description = options.getString("açıklama");
+      
+      
+       const embed = new MessageEmbed()
+                .setAuthor({
+                    name:`${guild.name} | Butonlu Rol Alma`,
+                    iconURL:guild.iconURL({dynamic:true})
+                })
+                .setDescription(description)
+                .setColor("GREEN");
+      
+const Buton = new MessageActionRow().addComponents(
+                    new MessageButton()
+                    .setCustomId("destek")
+                    .setLabel(`${buton1isim}`)
+                    .setStyle("PRIMARY")
+                    .setEmoji("")// İstediğin Emojiyi koy
+                )
+const Buton1 = new MessageActionRow().addComponents(
+                    new MessageButton()
+                    .setCustomId("destek")
+                    .setLabel(`${buton2isim}`)
+                    .setStyle("PRIMARY")
+                    .setEmoji("")// İstediğin Emojiyi koy
+                )
 }
 };
