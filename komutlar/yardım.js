cf9f5d0aa006d2ceb61e8da5d9cf1d4d2a85fa71
Glitch (harsh-married-toucan) <none>
2023-01-17 16:47:47 +0000

    🏎🍁 Checkpoint ./psychoconf/config.json:52832527/208 ./komutlar/yardım.js:52832527/441

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,17 +1,12 @@
 const { MessageEmbed,Client,CommandInteraction,MessageActionRow,MessageButton,Permissions } = require("discord.js");
-const {butonrol1,buton1isim,butonrol2,buton2isim} = require("../psychoconf/config.json");
+const {butonrol1,buton1isim,butonrol2,buton2isim,embedaçıklama1,embedaçıklama,emoji,emoji1} = require("../psychoconf/config.json");
 
 module.exports = {
     name:"gönder",
     description: 'gönder',
     type:1,
     options:[ 
-        {
-            name:"açıklama",
-            description:"Embed mesajın açıklaması",
-            type:3,
-            required:true
-    },
+        
      {
             name:"kanal",
             description:"Gönderilecek kanalı ayarlarsınız",
@@ -30,7 +25,6 @@ module.exports = {
  */
     run: async (client, interaction) => {
             const {guild, options} = interaction;
-            const description = options.getString("açıklama");
                   const channel = options.getChannel("kanal");
 
       
@@ -39,12 +33,12 @@ module.exports = {
                     name:`${guild.name} | Butonlu Rol Alma`,
                     iconURL:guild.iconURL({dynamic:true})
                 })
-                .setDescription(description)
+                .setDescription(`${emoji}-<@${butonrol1}> ${embedaçıklama} \r\n ${emoji1}-${buton1isim} ${embedaçıklama1}`)
                 .setColor("GREEN");
       
    const buton = new MessageActionRow().addComponents(
-            new MessageButton().setCustomId("1buton").setLabel(`${buton1isim}`).setStyle("PRIMARY").setEmoji("1️⃣"),
-            new MessageButton().setCustomId("2buton").setLabel(`${buton2isim}`).setStyle("PRIMARY").setEmoji("2️⃣"),)
+            new MessageButton().setCustomId("1buton").setLabel(`${buton1isim}`).setStyle("PRIMARY").setEmoji(`${emoji}`),
+            new MessageButton().setCustomId("2buton").setLabel(`${buton2isim}`).setStyle("PRIMARY").setEmoji(`${emoji}`),)
    
    
    
